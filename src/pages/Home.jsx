06d2668300b2799c839fff1b540fc7bce01ead03
abirtasrif <abirtasrif@gmail.com>
2023-01-26 17:16:30 +0600

    heor layout completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,22 @@ import { isWebpSupported } from "react-image-webp/dist/utils";
 const Home = () => {
   return (
     <div className="home container mx-auto flex flex-row mt-[5.5rem]">
-      <div className="hero pr-6">
+      <div className="hero pl-6 pt-24">
         <div className="hero-cta">
-          <h1 className="hero-cta-text uppercase text-6xl pt-10">
+          <h1 className="hero-cta-text uppercase text-7xl pt-9 leading-[4.5rem]">
             Save a life today.
             <br />
             Donate blood.
           </h1>
         </div>
-        <p className="hero-description">
+        <p className="hero-description w-1/2 text-justify py-5">
           Blood donation is a selfless act that can make a huge difference in
           someone's life. By donating blood, you can help save lives and improve
           the health of your community.
         </p>
-        <button>Join Our Program</button>
+        <button className="button py-3 px-4 mt-5 text-white bg-red-600 hover:bg-[#544440] duration-300">
+          Join Our Program
+        </button>
       </div>
       <div className="hero-img-container">
         {isWebpSupported() ? (
